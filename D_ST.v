02c39ddd4148984f48b4d86c429d94a57cc150b2
Charlotte2000s <56029662+Charlotte2000s@users.noreply.github.com>
2019-11-03 12:47:30 +0800

    Update D_ST.v

--- D_ST.v
+++ D_ST.v
@@ -1,1 +1,17 @@
+module D_ST(q, d, clk, reset, set);//synchronizer trigger
+	output q;
+	reg q;
+	input d,clk,reset,set;
+	
+	always@(posedge clk)
+		begin
+			if(reset == 1)
+					q <= 0;
+			else if(set == 1)
+					q <= 1;
+			else  
+					q <= d;
+					
+		end
+endmodule
 
