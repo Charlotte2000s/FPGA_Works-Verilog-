71b84ff488803c0d9956d09bda91232aefa51596
Charlotte2000s <56029662+Charlotte2000s@users.noreply.github.com>
2019-11-10 22:42:50 +0800

    Update 74LS374.v

--- 74LS374.v
+++ 74LS374.v
@@ -1,1 +1,21 @@
+module 74LS374(D,clk,en_able,Q);
+
+input [7:0] D;
+input en_able;
+input clk;
+
+output [7:0] Q;
+
+wire [7:0] D;
+reg [7:0] Q;
+
+always @ ( posedge clk )
+begin
+  if (en_able==0)
+     Q <= D;
+  else 
+    Q <= 8'bzzzzzzzz;
+end
+
+endmodule
 
