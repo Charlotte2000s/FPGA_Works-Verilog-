e684b9fd85dbbc78dd6c0dfb5ba49e78eb0081dc
Charlotte2000s <56029662+Charlotte2000s@users.noreply.github.com>
2019-11-10 22:40:09 +0800

    Update dff.v

--- dff.v
+++ dff.v
@@ -1,1 +1,18 @@
+module dff(clk, d, reset, q);
 
+input clk, d, reset;
+
+output q;
+
+reg q;
+
+always@(posedge clk)
+
+	begin 
+	if(reset)
+		q <= 1'b0; //时钟边沿到来且有复位信号，触发器被复位
+	else
+		q <= d;
+	end
+	
+endmodule
